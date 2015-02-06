b0f451ecd93d75022b401101ca46a02dbff3a5cd
Slavey Karadzhov <slavey.karadzhov@zend.com>
2015-02-06 18:04:16 +0100

    Initial version of the NodeMCU environment.

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,7 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.vm.box = "hashicorp/precise32"
+  config.vm.box = "ubuntu/trusty32"
   config.vm.provision :shell, path: "vm-bootstrap.sh", privileged: false
 
   config.vm.provider :virtualbox do |vb|
--- vm-bootstrap.sh
+++ vm-bootstrap.sh
@@ -14,32 +14,30 @@ fi
 sudo chown vagrant /opt/Espressif
 
 # Build the cross-compiler
-cd /opt/Espressif
-if [ ! -d /opt/Espressif/crosstool-NG ]; then
-	git clone -b lx106 git://github.com/jcmvbkbc/crosstool-NG.git
-fi
+cd /opt/
 
-cd /opt/Espressif/crosstool-NG
+IS_EMPTY=`find Espressif/ -maxdepth 0 -empty -exec echo -n 1 \;`
+if [ "$IS_EMPTY" == "1" ]; then
+	git clone https://github.com/pfalcon/esp-open-sdk.git Espressif
+fi
+cd Espressif
 git pull
+git submodule update
+# TODO: if the build fails try to clean the code by uncommenting the line below
+# make clean
+make STANDALONE=y
 
-if [ ! -d /opt/Espressif/crosstool-NG/builds ]; then
-	./bootstrap && ./configure -- prefix=`pwd` && make && make install
-	./ct-ng xtensa-lx106-elf
-fi
-
-
-CT_DEBUG_CT_SAVE_STEPS=1 ./ct-ng build
-PATH=$PWD/builds/xtensa-lx106-elf/bin:$PATH
+export PATH=$PWD/xtensa-lx106-elf/bin:$PATH
 
 # Setup the cross compiler
-HAS_PATH=`cat ~/.bashrc | grep "/opt/Espressif/crosstool-NG/builds/xtensa-lx106-elf/bin:" || echo -n ""`
+HAS_PATH=`cat ~/.bashrc | grep "$PWD/xtensa-lx106-elf/bin:" || echo -n ""`
 if [ -z $HAS_PATH ]; then
 	echo "# Add Xtensa Compiler Path" >> ~/.bashrc
-	echo "PATH=$PWD/builds/xtensa-lx106-elf/bin:$PATH" >> ~/.bashrc
+	echo "PATH=$PWD/xtensa-lx106-elf/bin:$PATH" >> ~/.bashrc
 fi
 
 
-cd /opt/Espressif/crosstool-NG/builds/xtensa-lx106-elf/bin
+cd $PWD/xtensa-lx106-elf/bin
 sudo rm -f xt-*
 for i in `ls xtensa-lx106*`; do
 	XT_NAME=`echo -n $i | sed s/xtensa-lx106-elf-/xt-/`
@@ -47,7 +45,7 @@ for i in `ls xtensa-lx106*`; do
 	sudo ln -s "$i" "$XT_NAME"; 
 done
 sudo ln -s xt-cc xt-xcc # the RTOS SDK needs it
-sudo chown vagrant -R /opt/Espressif/crosstool-NG/builds/xtensa-lx106-elf/bin
+sudo chown vagrant -R /opt/Espressif/xtensa-lx106-elf/bin
 
 HAS_CROSS_COMPILE=`cat ~/.bashrc | grep CROSS_COMPILE || echo -n ""`
 if [ -z $HAS_CROSS_COMPILE ]; then
@@ -55,66 +53,28 @@ if [ -z $HAS_CROSS_COMPILE ]; then
 	echo "CROSS_COMPILE=xtensa-lx106-elf-" >> ~/.bashrc
 fi
 
-# Set up the SDK
-cd /opt/Espressif
-LATEST_SDK_VERSION="esp_iot_sdk_v0.9.5"
-CURRENT_SDK_VERSION=`readlink esp8266_sdk || echo -n ""`;
-
-if [ "$LATEST_SDK_VERSION" != "$CURRENT_SDK_VERSION" ]; then
-	rm -rf esp8266_sdk
-	unzip -o /vagrant/tools/sdk/esp_iot_sdk_v0.9.5_15_01_23.zip
-	mv License esp_iot_sdk_v0.9.5/
-	mv release_note.txt esp_iot_sdk_v0.9.5/
-	cd esp_iot_sdk_v0.9.5/include
-	unzip -o /vagrant/tools/sdk/esp_iot_sdk_v0.9.5_15_01_23_patch1.zip  user_interface.h
-	cd ../lib
-	mv libmain.a libmain.a.old
-
-	unzip -o /vagrant/tools/sdk/esp_iot_sdk_v0.9.5_15_01_23_patch1.zip libmain_fix_0.9.5.a
-	mv libmain_fix_0.9.5.a libmain.a
-	cd ../../
-	ln -s esp_iot_sdk_v0.9.5 esp8266_sdk
-	cp /vagrant/tools/sdk/extra-libs/* esp8266_sdk/lib/
-	cd /opt/Espressif/esp8266_sdk
-	tar -xzf /vagrant/tools/sdk/extra-includes/include.tgz
-fi
-
 HAS_SDK_BASE=`cat ~/.bashrc | grep ESP8266_SDK_BASE || echo -n ""`
 if [ -z $HAS_SDK_BASE ]; then
 	echo "# ESP8266 SDK Base" >> ~/.bashrc
-	echo "ESP8266_SDK_BASE=/opt/Espressif/esp8266_sdk" >> ~/.bashrc
+	echo "ESP8266_SDK_BASE=/opt/Espressif/sdk" >> ~/.bashrc
 fi
-
-# Set up the RTOS SDK
-cd /opt/Espressif
-if [ ! -d /opt/Espressif/esp8266_rtos_sdk ]; then
-	git clone https://github.com/espressif/esp_iot_rtos_sdk.git esp8266_rtos_sdk
-	git clone https://github.com/espressif/esp_iot_rtos_sdk_lib esp8266_rtos_sdk_lib
-	cp esp8266_rtos_sdk_lib/lib/* esp8266_rtos_sdk/lib
-fi
-
-cd /opt/Espressif/esp8266_rtos_sdk
-git pull
-make 
-
-HAS_RTOS_SDK_BASE=`cat ~/.bashrc | grep ESP8266_RTOS_SDK_BASE || echo -n ""`
-if [ -z $HAS_RTOS_SDK_BASE ]; then
-	echo "# ESP8266 RTOS SDK Base" >> ~/.bashrc
-	echo "ESP8266_RTOS_SDK_BASE=/opt/Espressif/esp8266_rtos_sdk" >> ~/.bashrc
-fi
-
-
 
 # Install ESP tool
 sudo dpkg -i /vagrant/tools/esptool/esptool_0.0.2-1_i386.deb
 
 # Install esptool-py
-cd /opt/Espressif
-if [ ! -d /opt/Espressif/esptool-py ]; then
-	git clone https://github.com/themadinventor/esptool esptool-py
-fi
-cd /opt/Espressif/esptool-py
-git pull
 sudo rm -f /usr/local/bin/esptool.py
-sudo ln -s $PWD/esptool-py/esptool.py /usr/local/bin/
+sudo ln -s /opt/Espressif/esptool/esptool.py /usr/local/bin/
 
+
+# Compile the NodeMCU firmware
+if [ ! -d ~/dev ]; then
+	mkdir ~/dev
+fi
+cd ~/dev
+if [ ! -d ~/dev/nodemcu-firmware ]; then
+	git clone https://github.com/nodemcu/nodemcu-firmware.git
+fi
+cd nodemcu-firmware
+git pull
+make
